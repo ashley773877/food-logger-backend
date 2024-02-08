262cd866fb92161acfe98a44d22729c6dbfc4838
ashley773877 <mirandaashley16@gmail.com>
2024-02-08 11:34:03 -0500

    connected mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan'; 
+import './load.Env.js'
 
 
 
---
+++ load.Env.js
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//conection set up
